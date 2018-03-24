3c7c7b0428f80b36068a3b4845e73814b10c1dd9
Fedor Yatsenko <feda159357@gmail.com>
2018-03-24 11:04:58 +0200

    First commit

---
+++ CS_lab1_part2/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace CS_lab1_part2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOpenFile = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonOpenFile
+            // 
+            this.buttonOpenFile.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonOpenFile.Location = new System.Drawing.Point(150, 30);
+            this.buttonOpenFile.Name = "buttonOpenFile";
+            this.buttonOpenFile.Size = new System.Drawing.Size(130, 60);
+            this.buttonOpenFile.TabIndex = 0;
+            this.buttonOpenFile.Text = "Open file";
+            this.buttonOpenFile.UseVisualStyleBackColor = true;
+            this.buttonOpenFile.Click += new System.EventHandler(this.buttonOpenFile_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(250, 114);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Result:";
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelResult.Location = new System.Drawing.Point(20, 154);
+            this.labelResult.MaximumSize = new System.Drawing.Size(660, 0);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(56, 18);
+            this.labelResult.TabIndex = 2;
+            this.labelResult.Text = "result";
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonSave.Location = new System.Drawing.Point(370, 30);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(130, 60);
+            this.buttonSave.TabIndex = 0;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 761);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonOpenFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOpenFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Button buttonSave;
+    }
+}
+
---
+++ CS_lab1_part2/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CS_lab1_part2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOpenFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = @"E:\GoogleDisk\Study\Temp\Комп'ютерні системи\lab1";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string filename = openFileDialog1.FileName;
+
+                labelResult.Text = "";
+                length = 0;
+                ConvertFile(filename);
+            }
+        }
+
+        private static readonly int CHUNK_SIZE = 30720;
+
+        private void ConvertFile(string filename)
+        {
+            using (FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read))
+            {
+                using (BinaryReader br = new BinaryReader(fs))
+                {
+                    
+                    byte[] chunk;
+                    MyBuffer buffer = new MyBuffer(this);
+
+                    chunk = br.ReadBytes(CHUNK_SIZE);
+                    while (chunk.Length > 0)
+                    {
+                        buffer.Add(chunk);
+                        chunk = br.ReadBytes(CHUNK_SIZE);
+                    }
+                }
+            }
+        }
+
+        private int length;
+
+        private void PrintBuff(string s)
+        {
+            while(s.Length > 76 - length)
+            {
+                labelResult.Text += s.Substring(0, 76);
+                s = s.Remove(0, 76);
+
+                labelResult.Text += "\n";
+
+                length = 0;
+            }
+
+            length = s.Length;
+            labelResult.Text += s;
+        }
+
+        class MyBuffer
+        {
+            string code = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+            Form1 form1;
+
+            public MyBuffer(Form1 form1)
+            {
+                this.form1 = form1;
+            }
+
+            public void Add(byte[] byteArray)
+            {
+                /*
+                int singleCode = byteArray[0] * 65536 
+                    + (byteArray.Length > 1?byteArray[1]:0) * 256 
+                    + (byteArray.Length > 2 ? byteArray[2] : 0);
+
+                string result = code[singleCode / 262144].ToString()
+                    + code[singleCode / 4096 % 64]
+                    + (byteArray.Length > 1 ? code[singleCode % 4096 / 64].ToString(): "=")
+                    + (byteArray.Length > 2 ? code[singleCode % 64].ToString(): "=");
+                    */
+
+                string result = "";
+                for (int i = 0; i < byteArray.Length; i += 3)
+                {
+                    result += code[byteArray[i] >> 2].ToString();
+                    result += code[((byteArray[i] & 3) << 4) + (byteArray.Length > i + 1 ? byteArray[i + 1] >> 4 : 0)];
+                    result += (byteArray.Length > i + 1 ? code[((byteArray[i + 1] & 15) << 2) + (byteArray.Length > i + 2 ? byteArray[i + 2] >> 6 : 0)].ToString() : "=");
+                    result += (byteArray.Length > i + 2 ? code[byteArray[i + 2] & 63].ToString() : "=");
+                }
+
+                form1.PrintBuff(result);
+
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.Filter = "txt files (*.txt)|*.txt";
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string filename = saveFileDialog1.FileName;
+
+                using (StreamWriter sw = new StreamWriter(filename, false, System.Text.Encoding.ASCII))
+                {
+                    foreach (char c in labelResult.Text)
+                        if (c == '\n')
+                            sw.WriteLine();
+                        else
+                            sw.Write(c);
+
+                    MessageBox.Show("Success");
+                }
+            }
+        }
+    }
+}
